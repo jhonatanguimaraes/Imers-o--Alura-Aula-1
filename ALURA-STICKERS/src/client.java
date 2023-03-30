1f067d18b884961d7114ec37f4419f56529d9643
Jhonatan Guimaraes Rodrigues <guimaraesjhonatan87@gmail.com>
2023-03-30 13:15:20 -0400

    imersaoalura1

---
+++ ALURA-STICKERS/src/client.java
@@ -0,0 +1,11 @@
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandler;
+
+public class client {
+
+    public static HttpResponse<String> send(HttpRequest request, BodyHandler<String> ofString) {
+        return null;
+    }
+
+}
